354e269800e5782de473065c91b4bd2713e3dc3c
Gram <master_fess@mail.ru>
2018-12-27 19:01:44 +0300

    more 4 field

--- vv/field.py
+++ vv/field.py
@@ -1,24 +1,29 @@
 import re
-from typing import Optional, Any
-from inspect import formatannotation
+from typing import Optional, Any, List
 from enum import Enum
+from typing import Callable
 
-from .flags import NOTHING
+from .flags import NOTHING, default
 
 
 rex_optional = re.compile(r'Union\[.*\, NoneType\]')
+rex_type = type(rex_optional)
 
 
 class Field:
     slug: str               # example: 'first_name'
     name: str               # example: 'First Name'
     hint: Any               # example: Union[str, int]
+
     optional: bool = False
     choices: Optional[Enum] = None
     description: Optional[str] = None
+    default: Optional[Any] = None
+    validators: List[callable] = None
 
     def __init__(self, name: str, value: Any, hint: Any = NOTHING):
         self.name = self.slug = name
+        self.validators = []
 
         if not isinstance(value, (list, tuple)):
             value = (value, )
@@ -31,12 +36,21 @@ class Field:
         for rule in value:
             self._attach(rule)
 
-    def _attach(self, rule):
+    def _attach(self, rule: Any) -> None:
         if isinstance(rule, str):
             self.description = rule
             return
         if isinstance(rule, Enum):
             self.choices = rule
+            return
+        if isinstance(rule, default):
+            self.default = rule.value
+            return
+        if isinstance(rule, Callable):
+            self.validators.append(rule)
+            return
+        if isinstance(rule, rex_type):
+            self.validators.append(rule.fullmatch)
             return
 
     def __repr__(self) -> str:
--- vv/flags.py
+++ vv/flags.py
@@ -1,3 +1,9 @@
 
 
 NOTHING = object()
+optional = object()
+
+
+class default:  # noQA
+    def __init__(self, value):
+        self.value = value
